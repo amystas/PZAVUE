c6e95ef349ef8396efeed1ff3577b5d669e13e3f
AmyStas <95294347+amystas@users.noreply.github.com>
2024-02-18 21:34:30 +0100

    doc 14

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
-const message = ref('Hello Vue 3!');
+const name = ref('Amelia');
+
+name.value = 'Damian';
+
+const user = reactive({name: 'Amelia', surname: 'Staszczyk'});
+user.surname = 'Nowak';
+
+const selectedColor = ref();
 
 </script>
 
 <template>
   <div>
-    <h1>{{ message }}</h1>
-    <input type="text" v-model="message"/>
+    <h1>{{ name }}</h1>
+    <input type="text" v-bind:value="name" v-on:input="(event) => { name = event.target.value; }"/>
+    <input type="text" v-model="name" :input="(event) => { name = event.target.value; }"/>
+    <ul>
+      <li>{{ user.name }}</li>
+      <li>{{ user.surname }}</li>
+    </ul>
+    <select v-model="selectedColor" :on-change="(event) => {selectedColor = event.target.value;}">
+      <option value="red">red</option>
+      <option value="blue">blue</option>
+      <option value="green">green</option>
+    </select>
+    <p>Wybrany kolor: 
+      <span :style="{color: selectedColor}">{{ selectedColor }}</span>
+    </p>
   </div>
 </template>
